5dbe5dcbbe441c0d34e92c64ef4d7428577b5a1d
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-06 21:07:32 +0200

    Fix rubocop style complaint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: do
+  devise_for :users, controllers: {
     passwords: 'users/passwords',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
-  end
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
