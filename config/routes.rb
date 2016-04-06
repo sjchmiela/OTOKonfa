7a8d709895c51fe9a7b8abb625bed3b29ca5e566
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-06 21:03:57 +0200

    Generate scaffold controllers for users and use them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: do
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
