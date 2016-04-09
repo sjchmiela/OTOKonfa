d91448ec201cc090a9eac699d7fa364306a56fe5
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-09 16:15:22 +0200

    Fix rubocop warnings

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -66,13 +66,14 @@ class VenuesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_venue
-      @venue = Venue.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def venue_params
-      params.require(:venue).permit(:name, :description, :address, :geoposition, :phone)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def venue_params
+    params.require(:venue).permit(:name, :description, :address, :geoposition, :phone)
+  end
 end
