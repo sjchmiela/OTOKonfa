234d2d4c376a283ba727bd7da4d7cd62623487f5
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-12 16:06:28 +0200

    Add association to image and validations in Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
+  has_attached_file :image
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, content_type: /\Aimage/
+  validates_attachment_size :image, in: 0..1.megabytes
 end
