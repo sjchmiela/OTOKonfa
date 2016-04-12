51f3bc45dbff371c5bc4e656ddef189667114695
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-12 23:03:08 +0200

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   # Removing :validatable => accounts won't have to be confirmed.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_confirmation_of :password
+  validates_length_of :first_name, in: 2..100
+  validates_length_of :last_name, in: 2..100
 end
