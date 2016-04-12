722edfdd7c44213ce524f071b091030fca6414ca
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-12 10:35:53 +0200

    Migrate database changes – create photos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409135126) do
+ActiveRecord::Schema.define(version: 20160412083038) do
 
   create_table "halls", force: :cascade do |t|
     t.integer  "venue_id",    limit: 4
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20160409135126) do
   end
 
   add_index "hotels", ["venue_id"], name: "index_hotels_on_venue_id", using: :btree
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
+    t.string   "title",              limit: 255
+    t.integer  "imageable_id",       limit: 4
+    t.string   "imageable_type",     limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "room_components", force: :cascade do |t|
     t.integer  "hotel_id",   limit: 4
