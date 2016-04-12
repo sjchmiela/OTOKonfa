4a28c87c90e9ddd198e8cb9cfe08b8b0c15d43ba
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-12 10:38:53 +0200

    Add order:integer field to photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412083038) do
+ActiveRecord::Schema.define(version: 20160412083832) do
 
   create_table "halls", force: :cascade do |t|
     t.integer  "venue_id",    limit: 4
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160412083038) do
     t.string   "imageable_type",     limit: 255
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.integer  "order",              limit: 4
   end
 
   create_table "room_components", force: :cascade do |t|
