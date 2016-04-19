f4345148dc9fdbb0de81ec84d424ae67aca14480
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-19 19:22:42 +0200

    Add validations to Manager

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -3,4 +3,11 @@ class Manager < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_confirmation_of :password
+  validates_length_of :first_name, in: 2..100
+  validates_length_of :last_name, in: 2..100
+
+  has_many :venues
 end
