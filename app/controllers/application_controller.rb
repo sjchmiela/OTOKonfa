2c661fc8260d040c69c04feb5baa4afed9b9b519
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-20 19:08:33 +0200

    Logout user if manager is logged in, fixes #15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :logout_user_if_manager_logged_in
 
+  def logout_user_if_manager_logged_in
+    sign_out(current_user) if manager_signed_in? && user_signed_in?
+  end
 end
