84011748a16f736717c6e1b1bc5bdce7af2993c1
Pawel Wolak <pawelwolakk@gmail.com>
2016-04-26 22:56:42 +0200

    added search component

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -1,6 +1,10 @@
 (function($){
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
-    $('.page-nav ul').pushpin({ top: $('.page-nav').offset().top, offset: $('.page-nav').offset().top});
+    $('.page-nav ul').pushpin({
+        top: $('.page-nav').offset().top,
+        offset: $('.page-nav').offset().top,
+        bottom: $('.default-page').next().offset().top - $('.page-nav ul').height() - 45
+    });
     $('.default-page').find('h3, h4, h5, h6').scrollSpy();
 })(jQuery);
