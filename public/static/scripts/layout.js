7de1b39e6503d61379da990e3cfffde90641bcf3
Pawel Wolak <pawelwolakk@gmail.com>
2016-04-26 21:29:52 +0200

    added default text page

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -1,4 +1,6 @@
 (function($){
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
+    $('.page-nav ul').pushpin({ top: $('.page-nav').offset().top, offset: $('.page-nav').offset().top});
+    $('.default-page').find('h3, h4, h5, h6').scrollSpy();
 })(jQuery);
