358bba0112d176b3a865ac2fde56e0e98bcf1978
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-06 13:10:08 +0200

    moved code to onload handler

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -1,31 +1,33 @@
 (function($){
-    $('.parallax').parallax();
-    $('.modal-trigger').leanModal();
+    $(document).ready(function(){
+        $('.parallax').parallax();
+        $('.modal-trigger').leanModal();
 
-    var $pageNav = $('.page-nav');
-    var $defaultPage = $('.default-page');
+        var $pageNav = $('.page-nav');
+        var $defaultPage = $('.default-page');
 
-    if($pageNav.size()){
-        $pageNav.find('ul').pushpin({
-            top: $pageNav.offset().top,
-            offset: $pageNav.offset().top,
-            bottom: $defaultPage.next().offset().top - $pageNav.find('ul').height() - 45
+        if($pageNav.size()){
+            $pageNav.find('ul').pushpin({
+                top: $pageNav.offset().top,
+                offset: $pageNav.offset().top,
+                bottom: $defaultPage.next().offset().top - $pageNav.find('ul').height() - 45
+            });
+            $defaultPage.find('h3, h4, h5, h6').scrollSpy();
+        }
+
+        $('.range-input').each(function(){
+            noUiSlider.create($(this).get(0), {
+                start: [$(this).data('start-from'), $(this).data('start-to')],
+                connect: true,
+                step: 1,
+                range: {
+                    'min': $(this).data('min'),
+                    'max': $(this).data('max')
+                },
+                format: wNumb({
+                    decimals: 0
+                })
+            });
         });
-        $defaultPage.find('h3, h4, h5, h6').scrollSpy();
-    }
-
-    $('.range-input').each(function(){
-        noUiSlider.create($(this).get(0), {
-            start: [$(this).data('start-from'), $(this).data('start-to')],
-            connect: true,
-            step: 1,
-            range: {
-                'min': $(this).data('min'),
-                'max': $(this).data('max')
-            },
-            format: wNumb({
-                decimals: 0
-            })
-        });
-    })
+    });
 })(jQuery);
