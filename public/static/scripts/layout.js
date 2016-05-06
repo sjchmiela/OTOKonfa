79a87fd7f4de6ed0830d93eebac01efd80b544b6
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-06 13:08:24 +0200

    added range input

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -13,4 +13,19 @@
         });
         $defaultPage.find('h3, h4, h5, h6').scrollSpy();
     }
+
+    $('.range-input').each(function(){
+        noUiSlider.create($(this).get(0), {
+            start: [$(this).data('start-from'), $(this).data('start-to')],
+            connect: true,
+            step: 1,
+            range: {
+                'min': $(this).data('min'),
+                'max': $(this).data('max')
+            },
+            format: wNumb({
+                decimals: 0
+            })
+        });
+    })
 })(jQuery);
