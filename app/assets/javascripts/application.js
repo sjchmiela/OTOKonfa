2700c0072a15799c87057e9221abba24b21441cd
Stanisław Chmiela <sjchmiela@gmail.com>
2016-05-07 22:57:35 +0200

    Copy assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+(function($){
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.page-nav ul').pushpin({
+        top: $('.page-nav').offset().top,
+        offset: $('.page-nav').offset().top,
+        bottom: $('.default-page').next().offset().top - $('.page-nav ul').height() - 45
+    });
+    $('.default-page').find('h3, h4, h5, h6').scrollSpy();
+})(jQuery);
