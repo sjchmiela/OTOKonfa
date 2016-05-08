43db2e6396e17e75a79caff01edb126e230eac89
Stanisław Chmiela <sjchmiela@gmail.com>
2016-05-08 22:24:43 +0200

    Add association to features in venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,6 +4,7 @@ class Venue < ActiveRecord::Base
   has_many :halls
   has_many :photos, as: :imageable
   has_many :reviews
+  has_and_belongs_to_many :features
 
   def average_rating
     return 0 if reviews.count == 0
