6bd6dc78abc6a0bdde135329f8389f0270852a80
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-09 00:13:39 +0200

    added geolocation

--- public/static/scripts/filter.js
+++ public/static/scripts/filter.js
@@ -1,5 +1,14 @@
 (function($){
+    var $location;
+    var $map;
+    var map;
+    var circle;
+    var marker;
+
     $(document).ready(function(){
+        $map = $('.map');
+        $location = $('#filter-location');
+
         $('.range-input').each(function(){
             noUiSlider.create($(this).get(0), {
                 start: [$(this).data('start-from'), $(this).data('start-to')],
@@ -17,18 +26,20 @@
     });
 
     window.initMap = function(){
-        var $map = $('.map');
-        var position = {lat: 50.060688, lng: 19.9304733};
-        var map = new google.maps.Map($map.get(0), {
+        var position = getDefaultPosition();
+
+        map = new google.maps.Map($map.get(0), {
             center: position,
             zoom: 12
         });
-        var marker = new google.maps.Marker({
+
+        marker = new google.maps.Marker({
             position: position,
             map: map,
             draggable: true
         });
-        var circle = new google.maps.Circle({
+
+        circle = new google.maps.Circle({
             map: map,
             radius: 1000,
             strokeWeight: 1,
@@ -36,5 +47,24 @@
             strokeColor: '#0288d1'
         });
         circle.bindTo('center', marker, 'position');
+    };
+
+    function getDefaultPosition(){
+        if($location.val() != ''){
+            var parts = $location.val().split(',');
+            return {lat: parts[0], lng: parts[1]};
+        } else {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(displayCurrentPosition);
+            }
+
+            return {lat: 50.060688, lng: 19.9304733};
+        }
+    }
+
+    function displayCurrentPosition(position){
+        var location = {lat: position.coords.latitude, lng: position.coords.longitude};
+        map.setCenter(location);
+        marker.setPosition(location);
     }
 })(jQuery);
