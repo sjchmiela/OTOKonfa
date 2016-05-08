56ff598659ed9cefeb394ad74bb4df24b2e1ab41
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-08 23:52:04 +0200

    added google map

--- public/static/scripts/filter.js
+++ public/static/scripts/filter.js
@@ -15,4 +15,18 @@
             });
         });
     });
+
+    window.initMap = function(){
+        var $map = $('.map');
+        var position = {lat: 50.060688, lng: 19.9304733};
+        var map = new google.maps.Map($map.get(0), {
+            center: position,
+            zoom: 12
+        });
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            draggable: true
+        });
+    }
 })(jQuery);
