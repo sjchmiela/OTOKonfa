63591c02c48ecfffdf5b3595b459dd19c1ad41b2
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-08 23:14:48 +0200

    decomposed scripts

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -2,32 +2,5 @@
     $(document).ready(function(){
         $('.parallax').parallax();
         $('.modal-trigger').leanModal();
-
-        var $pageNav = $('.page-nav');
-        var $defaultPage = $('.default-page');
-
-        if($pageNav.size()){
-            $pageNav.find('ul').pushpin({
-                top: $pageNav.offset().top,
-                offset: $pageNav.offset().top,
-                bottom: $defaultPage.next().offset().top - $pageNav.find('ul').height() - 45
-            });
-            $defaultPage.find('h3, h4, h5, h6').scrollSpy();
-        }
-
-        $('.range-input').each(function(){
-            noUiSlider.create($(this).get(0), {
-                start: [$(this).data('start-from'), $(this).data('start-to')],
-                connect: true,
-                step: 1,
-                range: {
-                    'min': $(this).data('min'),
-                    'max': $(this).data('max')
-                },
-                format: wNumb({
-                    decimals: 0
-                })
-            });
-        });
     });
 })(jQuery);
