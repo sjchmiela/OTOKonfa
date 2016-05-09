fc01f4d7cc3e87704de800b944b73f5a10feaca2
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-09 23:08:10 +0200

    added edit enable button

--- public/static/scripts/venue.js
+++ public/static/scripts/venue.js
@@ -2,13 +2,70 @@
     var $map;
     var map;
     var marker;
+    var saved;
+    var editEnabled = false;
+    var MESSAGES = {
+        SAVING: 'Zapisuję zmiany...',
+        DONE: 'Gotowe!',
+        ERROR: 'Błąd!'
+    };
+    var $toggle;
 
     $(document).ready(function(){
         $map = $('.map');
+        $toggle = $('.edit-venue > span');
+
+        $('body')
+            .on('focus', '[contenteditable]', onFocus)
+            .on('blur', '[contenteditable]', onBlur)
+            .on('click', '.edit-venue', toggleEditMode);
     });
 
+    function onFocus(){
+        saved = $(this).text();
+    }
+
+    function onBlur(){
+        var current = $(this).text();
+
+        if(current != saved){
+            save( $(this).data('property'), current );
+        }
+    }
+
+    function toggleEditMode(){
+        var add, remove, text, fn;
+
+        if(editEnabled){
+            add = 'red';
+            remove = 'green';
+            text = 'mode_edit';
+            fn = function(){
+                if(!$(this).hasClass('map')){
+                    $(this).removeAttr('contenteditable');
+                }
+            };
+        } else {
+            add = 'green';
+            remove = 'red';
+            text = 'done';
+            fn = function(){
+                if(!$(this).hasClass('map')){
+                    $(this).attr('contenteditable', true);
+                }
+            };
+        }
+
+        $toggle.removeClass(remove).addClass(add).find('i').text(text);
+
+        editEnabled = !editEnabled;
+
+        $('[data-property]').each(fn);
+    }
+
     window.initMap = function(){
-        var position = {lat: 50.060688, lng: 19.9304733};
+        var data = $map.data('geoposition').split(',');
+        var position = {lat: parseFloat(data[0]), lng: parseFloat(data[1])};
 
         map = new google.maps.Map($map.get(0), {
             center: position,
@@ -20,5 +77,17 @@
             map: map,
             draggable: true
         });
+
+        google.maps.event.addListener(marker, 'dragend', updateLocation );
     };
+
+    function updateLocation(){
+        var location = marker.getPosition();
+        var data = location.lat() + ',' + location.lng();
+        map.setCenter(location);
+        save( $map.data('property'), data );
+    }
+
+    function save(property, value){
+    }
 })(jQuery);
