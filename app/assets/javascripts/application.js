c9d530e75246d3fec99bdb1377193d809facbcd9
Stanisław Chmiela <sjchmiela@gmail.com>
2016-05-09 19:34:53 +0200

    Remove obsolete initializers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,16 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require typahead.bundle
 //= require materialize-sprockets
 //= require_tree .
-
-(function($){
-    $('.parallax').parallax();
-    $('.modal-trigger').leanModal();
-    $('.page-nav ul').pushpin({
-        top: $('.page-nav').offset().top,
-        offset: $('.page-nav').offset().top,
-        bottom: $('.default-page').next().offset().top - $('.page-nav ul').height() - 45
-    });
-    $('.default-page').find('h3, h4, h5, h6').scrollSpy();
-})(jQuery);
