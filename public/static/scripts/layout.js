7c965df66a4342a57ce4a3aeb4a5cbd28b90f82b
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-10 15:16:02 +0200

    added login handler

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -2,5 +2,15 @@
     $(document).ready(function(){
         $('.parallax').parallax();
         $('.modal-trigger').leanModal();
+
+        $('#modal-login').on('submit', 'form', function(e){
+            e.preventDefault();
+
+            $.post( $(this).attr('action'), $(this).serialize()).done(function(){
+                window.location.reload();
+            }).fail(function(result){
+                Materialize.toast(result.error, 4000, 'red');
+            });
+        });
     });
 })(jQuery);
