836d8c3aea476420340119fe0321451406505daf
Pawel Wolak <pawelwolakk@gmail.com>
2016-05-10 21:40:24 +0200

    added register form support

--- public/static/scripts/layout.js
+++ public/static/scripts/layout.js
@@ -12,5 +12,16 @@
                 Materialize.toast(result.message, 4000, 'red');
             });
         });
+
+        $('#modal-register').on('submit', 'form', function(e){
+            e.preventDefault();
+
+            $.post( $(this).attr('action'), $(this).serialize()).done(function(result){
+                $('#modal-register').closeModal();
+                Materialize.toast(result.message, 4000, 'green');
+            }).fail(function(result){
+                Materialize.toast(result.message, 4000, 'red');
+            });
+        });
     });
 })(jQuery);
