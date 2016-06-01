0cd361013bbbf82e2c3565971c06a03305f3f9d5
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-01 18:49:14 +0200

    Minor fixes

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,5 @@
 class Venue < ActiveRecord::Base
-  belongs_to :manager  
+  belongs_to :manager
   validates_presence_of :email
   has_many :hotels
   has_many :halls
@@ -14,12 +14,12 @@ class Venue < ActiveRecord::Base
 
   def city
     return '' if address.nil?
-    (address).split('\n').last.match('(.)+\s')
+    (address).split(', ').last
   end
 
   def hotel_size
-    hotels.to_a.inject(0) do |hotel, sum|
-      hotel.to_a.room_components.inject(0) do |room, sum_hotel|
+    self.hotels.to_a.inject(0) do |sum, hotel|
+      hotel.room_components.to_a.inject(0) do |sum_hotel, room|
         (room.quantity * room.capacity) + sum_hotel
       end
       + sum
