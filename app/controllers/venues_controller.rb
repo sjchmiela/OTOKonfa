13f7e420640373a8026f1a8c54cba8a81ff3ee14
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-01 18:48:19 +0200

    Fix

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,7 +7,7 @@ class VenuesController < ApplicationController
   # GET /venues.json
   def index
     venues = Venue.
-      where('name like ? or description like ?', "%#{params[:search]}%", "%#{params[:search]}%").
+      where('name like ? or description like ?', "%#{params[:query]}%", "%#{params[:query]}%").
       includes(:halls, :hotels, :features)
 
     venues = venues.where(accepted: true) unless admin_signed_in
@@ -17,7 +17,7 @@ class VenuesController < ApplicationController
     # puts "array"
     # puts venues
 
-    if params[:hall_size]
+    if params[:hall_size] and !params['slot-type'].nil?
 
       min_places, max_places = params[:hall_size].split(',').map { |e| Integer(e) }
 
@@ -30,15 +30,15 @@ class VenuesController < ApplicationController
           params['slot-type'] == '0' ? h.chairs : h.capacity
         end
 
-        # puts biggest_hall, smallest_hall
 
-        true if min_places == 0 && biggest_hall.nil?
-        false if biggest_hall.nil?
-
-        if params['slot-type'] == '0'
-          (biggest_hall.chairs <= max_places && smallest_hall.chairs >= min_places)
+        if biggest_hall.nil? || smallest_hall.nil?
+          (min_places == 0)
         else
-          (biggest_hall.capacity <= max_places && smallest_hall.capacity >= min_places)
+          if params['slot-type'] == '0'
+            (biggest_hall.chairs <= max_places && smallest_hall.chairs >= min_places)
+          else
+            (biggest_hall.capacity <= max_places && smallest_hall.capacity >= min_places)
+          end
         end
       end
     end
@@ -59,8 +59,8 @@ class VenuesController < ApplicationController
       min_hotel_size, max_hotel_size = params[:hotel_size].split(',').map { |e| Integer(e) }
 
       venues = venues.keep_if do |venue|
-        hotels_size = venue.hotels.to_a.inject(0) do |hotel, sum|
-          hotel.to_a.room_components.inject(0) do |room, sum_hotel|
+        hotels_size = venue.hotels.to_a.inject(0) do |sum, hotel|
+          hotel.room_components.to_a.inject(0) do |sum_hotel, room|
             (room.quantity * room.capacity) + sum_hotel
           end
           + sum
