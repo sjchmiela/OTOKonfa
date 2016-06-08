80bc72ad23ab4d44fc34c757a08f81d86d1d3f45
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-08 22:26:35 +0200

    Add login integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,21 @@ class User < ActiveRecord::Base
   validates_length_of :last_name, in: 2..100
 
   has_many :reviews
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.public_profile.first_name
+      user.last_name = auth.info.public_profile.last_name
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
