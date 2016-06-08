322b1a95d2d76e21a92560ace62e61c7a39706b7
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-08 22:29:21 +0200

    Possibly fix nilClass error

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,8 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.first_name = auth.info.public_profile.first_name
-      user.last_name = auth.info.public_profile.last_name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
     end
   end
 
