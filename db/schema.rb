507e62e29328ee4dd3e82a641f8fe2b0672f39fc
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-08 22:25:54 +0200

    Add Omniauth columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521184948) do
+ActiveRecord::Schema.define(version: 20160608201209) do
 
   create_table "features", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -121,6 +121,8 @@ ActiveRecord::Schema.define(version: 20160521184948) do
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
     t.boolean  "admin",                              default: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
