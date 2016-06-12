5b0e703acd86ff1805809d0c0f8a23ae1c800118
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-12 23:04:45 +0200

    Fix wrong override

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   default_scope { order(:order) }
   belongs_to :imageable, polymorphic: true
-  has_attached_file :image, path: ":rails_root/storage/#{Rails.env}#{ENV['RAILS_TEST_NUMBER']}/attachments/:id/:style/:basename.:extension"
+  has_attached_file :image
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: /\Aimage/
   validates_attachment_size :image, in: 0..1.megabytes
