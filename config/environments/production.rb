bd37e9ed0417a7e1280fe9396c60e699ae76da88
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-12 18:54:02 +0200

    Set proper paperclip defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: 'otokonfa',
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    },
+    url: ":s3_domain_url",
+    :path => "/:class/:id_:basename.:style.:extension"
+  }
 end
